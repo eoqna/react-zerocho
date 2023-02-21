a6575db5e3d81ac5893ded60011c31dda4ab4444
eoqna <eoqna@naver.com>
2023-02-21 19:00:36 +0900

    [eoqna] 23-02-21 ZeroCho React Commit

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,13 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom/client');
 
-<<<<<<< HEAD
 const RenderTest = require('./RenderTest');
 
 ReactDOM.createRoot(document.querySelector('#root')).render(<RenderTest />);
-=======
-const WordRelay = require('./WordRelay');
-
-
-ReactDOM.createRoot(document.querySelector('#root')).render(<WordRelay />);  
->>>>>>> fcf628f958616a9be19d68fcde73b97036969842
